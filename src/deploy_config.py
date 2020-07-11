89a9b9b32958e3640cf4660adc6369ff31c4e10a
jpcorreia99 <jpcorreia99@gmail.com>
2020-07-11 18:48:58 +0100

    update db URI

--- src/deploy_config.py
+++ src/deploy_config.py
@@ -1,8 +1,7 @@
 from sqlalchemy import false
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.dd'
-SQLALCHEMY_DATABASE_URI = 'postgres://saspjteooejvzy:88d28e7e1183f84d651c5592cea2383a0113d4c5b0c8f3175eefb25fb4a18465' \
-                          '@ec2-18-211-48-247.compute-1.amazonaws.com:5432/dfb47qre2auqtp '
+SQLALCHEMY_DATABASE_URI = 'postgres://saspjteooejvzy:88d28e7e1183f84d651c5592cea2383a0113d4c5b0c8f3175eefb25fb4a18465@ec2-18-211-48-247.compute-1.amazonaws.com:5432/dfb47qre2auqtp'
 SQLALCHEMY_TRACK_MODIFICATIONS = false
 SECRET_KEY = 'THIS_IS_SECRET'
 
